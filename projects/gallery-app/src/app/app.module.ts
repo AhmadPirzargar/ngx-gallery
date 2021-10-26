2390d04625744e887f49fa7a4bdc0ad4c93f342a
ahmad <ahmad.pirzargar@gmail.com>
2021-10-26 22:16:23 +0330

    Remove BrowserAnimationsModule from lib

--- projects/gallery-app/src/app/app.module.ts
+++ projects/gallery-app/src/app/app.module.ts
@@ -1,9 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgxGalleryModule } from '@artavil/ngx-gallery';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -11,7 +11,7 @@ import { NgxGalleryModule } from '@artavil/ngx-gallery';
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserAnimationsModule,
     NgxGalleryModule,
     AppRoutingModule
   ],
